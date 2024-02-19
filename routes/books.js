5c61b0ceb61b9109f5f7310f477a2ae0d9be14aa
shokhruzisroilov <shokhruzisroilov@gmail.com>
2024-02-19 12:19:18 +0500

    Joi validate added

--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const Joi = require('joi')
 const OwaBooks = require('../models/Books')
 
 const router = express.Router()
 
+// Create Book
 router.post('/', async (req, res) => {
+	const { error } = validateBook(req.body)
+	if (error) {
+		return res.status(400).send(error.details[0].message)
+	}
 	try {
 		const book = new OwaBooks({
 			title: req.body.title,
@@ -24,5 +30,22 @@ router.post('/', async (req, res) => {
 		res.status(500).send(error.message)
 	}
 })
+
+// Validate Book
+function validateBook(book) {
+	const bookSchema = Joi.object({
+		title: Joi.string().required(),
+		description: Joi.string().required(),
+		author: Joi.string().required(),
+		aboutAuthor: Joi.string().required(),
+		rate: Joi.number().integer().min(1).max(5).required(),
+		img: Joi.string().uri().required(),
+		like: Joi.boolean().required(),
+		editionNumber: Joi.string().required(),
+		currentlyReading: Joi.number().integer().required(),
+		haveRead: Joi.number().integer().min(0).required(),
+	})
+	return bookSchema.validate(book)
+}
 
 module.exports = router
